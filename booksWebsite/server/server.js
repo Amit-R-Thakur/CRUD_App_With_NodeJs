946de1aa074aec4935c812fd046cc97786dc8458
Amit_R_Thakur <at913513@gmail.com>
2021-10-23 01:03:44 +0530

    Partial Added

--- booksWebsite/server/server.js
+++ booksWebsite/server/server.js
@@ -9,7 +9,23 @@ app.use(express.static(publicPath))
 app.set("view engine","ejs");
 app.get("/",(req,res)=>{
     // res.send("hello")
-    res.render("nevbar.ejs")
+    res.render("partial/nevbar.ejs")
+})
+app.get("/add",(req,res)=>{
+    // res.send("hello")
+    res.render("bookadd.ejs")
+})
+app.get("/update",(req,res)=>{
+    // res.send("hello")
+    res.render("bookupdate.ejs")
+})
+app.get("/delete",(req,res)=>{
+    // res.send("hello")
+    res.render("bookdelete.ejs")
+})
+app.get("/read",(req,res)=>{
+    // res.send("hello")
+    res.render("books.ejs")
 })
 // app.use(express.static())
 app.listen(port,()=>{
