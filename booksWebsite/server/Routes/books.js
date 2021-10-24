f9dafa6fdcb1446e3dc6ae506ac1a0433d6d7d0c
Amit_R_Thakur <at913513@gmail.com>
2021-10-24 14:41:37 +0530

    Book CRUD App Done

--- booksWebsite/server/Routes/books.js
+++ booksWebsite/server/Routes/books.js
@@ -14,33 +14,70 @@ const storages = multer.diskStorage({
 });
 const upload = multer({ storage: storages });
 Router.get("/add", (req, res) => {
-  // res.send("hello")
   res.render("bookadd.ejs");
 });
 Router.post("/add", upload.single("image"), (req, res) => {
-//   console.log(req.file.filename);
-  const image=req.file.filename
-  const {name,author,publisher,price}=req.body
-  const newBook = new book({name,author,publisher,price,image});
-   newBook.save()
+  const image = req.file.filename;
+  const { name, author, publisher, price } = req.body;
+  const newBook = new book({ name, author, publisher, price, image });
+  newBook.save();
   res.redirect("/");
 });
 
-
-
-Router.get("/update", (req, res) => {
-  // res.send("hello")
-  res.render("bookupdate.ejs");
+Router.get("/update", async (req, res) => {
+  const data = await book.find();
+  res.render("bookupdate.ejs", { data });
 });
-Router.get("/delete", async(req, res) => {
-  const data=await book.find()
-   console.log(data)
-  res.render("bookdelete.ejs",{data});
+Router.get("/update:id", async (req, res) => {
+  const _id = req.params.id;
+  const data = await book.findById(_id);
+  res.render("bookupdatefile", { data });
 });
-Router.get("/read", async(req, res) => {
-   const data=await book.find()
-   console.log(data)
-   
-  res.render("books.ejs",{data});
+Router.post("/update:id", upload.single("image"), async (req, res) => {
+  const _id = req.params.id;
+  console.log(req.body);
+  if (req.file) {
+    const image = req.file.filename;
+    const { name, author, publisher, price } = req.body;
+    const newRecord = { name, author, publisher, price, image };
+    await book
+      .updateOne({ _id }, { $set: newRecord }, { returnNewDocument: true })
+      .then(() => {
+        console.log("successfull");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    res.redirect("/update");
+  } else {
+    const { name, author, publisher, price } = req.body;
+    const newRecord = { name, author, publisher, price };
+    await book
+      .updateOne({ _id }, { $set: newRecord }, { returnNewDocument: true })
+      .then(() => {
+        console.log("successfull");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    res.redirect("/update");
+  }
+});
+
+Router.get("/delete", async (req, res) => {
+  const data = await book.find();
+  console.log(data);
+  res.render("bookdelete.ejs", { data });
+});
+Router.get("/delete:id", async (req, res) => {
+  const _id = req.params.id;
+  await book.findOneAndDelete({ _id });
+  res.redirect("/delete");
+});
+Router.get("/read", async (req, res) => {
+  const data = await book.find();
+  console.log(data);
+
+  res.render("books.ejs", { data });
 });
 module.exports = Router;
--- booksWebsite/server/server.js
+++ booksWebsite/server/server.js
@@ -1,27 +1,22 @@
 const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
-// const bodyParser = require("body-parser");
 require("../database/connect.js");
 const publicPath = path.join(__dirname, "../public");
 console.log(publicPath);
 const app = express();
 const port = 4000;
-
-app.use(express.static(path.join(__dirname, "../images")))
-// app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, "../images")));
 app.use(express.json());
-// app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 const book = require("./Routes/books.js");
 app.use(book);
 app.use(express.static(publicPath));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  // res.send("hello")
   res.render("partial/nevbar.ejs");
 });
-// app.use(express.static())
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
